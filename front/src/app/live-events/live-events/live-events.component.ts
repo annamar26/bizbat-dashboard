121abf6dd3e10eb0100506b216ed622e387c3c22
Emiliano <TE.EA2356@lanvera.org>
2023-08-28 19:44:33 -0600

    camios en live events

--- front/src/app/live-events/live-events/live-events.component.ts
+++ front/src/app/live-events/live-events/live-events.component.ts
@@ -6,14 +6,24 @@ import { DataService } from 'src/app/services/data.service';
   templateUrl: './live-events.component.html',
   styleUrls: ['./live-events.component.scss']
 })
-export class LiveEventsComponent implements OnInit {
+export class LiveEventsComponent {
 
   public listData: Array<object> = [];
+  liveEvents: any = [];
+  path?: string;
 
-  constructor(private _dataService: DataService) { }
-
-  ngOnInit() {
-    this.listData = this._dataService.getData();
+  constructor(private _dataService: DataService) {
+    this._dataService.liveEventsSubject.subscribe(path => {
+      this.path = path;
+      this.getData();
+    })
   }
+
+  getData(): void {
+    this._dataService.getDummy(this.path!).subscribe((res: any)=>{
+      this.liveEvents = res
+      console.log(this.liveEvents)
+  })}
+
 
 }
