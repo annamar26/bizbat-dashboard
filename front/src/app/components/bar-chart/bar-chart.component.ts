f799eec4791a6e2a16c940f077e2ba55425f5db3
Emiliano <TE.EA2356@lanvera.org>
2023-08-28 19:50:58 -0600

    agrego on destroy method

--- front/src/app/components/bar-chart/bar-chart.component.ts
+++ front/src/app/components/bar-chart/bar-chart.component.ts
@@ -1,56 +1,64 @@
-import { Component, Input, OnInit, Renderer2, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  SimpleChanges,
+} from '@angular/core';
 import { Chart, ChartTypeRegistry } from 'chart.js';
 import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.scss']
+  styleUrls: ['./bar-chart.component.scss'],
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnDestroy{
   barChart!: Chart;
-  @Input() label: string ='views';
+  @Input() label: string = 'views';
   @Input() data!: any[];
 
-
-  constructor( public renderer: Renderer2, private dataService: DataService){
-
+  constructor(public renderer: Renderer2, private dataService: DataService) {}
+  ngOnDestroy(): void {
+    if (this.barChart) {
+      this.barChart.destroy();
+    }
   }
 
   ngOnInit(): void {
-this.dataService.optionSubject.subscribe(res=>{
-    this.label = res
-    console.log(this.label)
-  })
-
-
+    this.dataService.optionSubject.subscribe((res) => {
+      this.label = res;
+      console.log(this.label);
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges): void{
-  this.data = changes?.['data'].currentValue
+  ngOnChanges(changes: SimpleChanges): void {
+    this.data = changes?.['data'].currentValue;
 
-  this.createBarChart()
-}
+    this.createBarChart();
+  }
 
   createBarChart() {
-
     if (this.barChart) {
       this.barChart.destroy();
-  }
-    const canvas: HTMLCanvasElement = this.renderer.selectRootElement('.barChartCanvas')
+    }
+    const canvas: HTMLCanvasElement =
+      this.renderer.selectRootElement('.barChartCanvas');
     const ctx = canvas.getContext('2d');
-    const colors = ['#E5399D', '#6742F5', '#20269E', '#0E0821']
+    const colors = ['#E5399D', '#6742F5', '#20269E', '#0E0821'];
 
     if (ctx) {
- 
       const userLabels = this.data.map((user: any) => user.id);
       const userFollowers = this.data.map((user: any) => user.total);
       const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-      console.log('label', this.label)
+      console.log('label', this.label);
 
       // colors.forEach((color: string, index: number) =>  {gradient.addColorStop(index, color)})
 
       this.barChart = new Chart(ctx, {
-        type: this.label.includes('top') ? 'bar': 'pie' as keyof ChartTypeRegistry,
+        type: this.label.includes('top')
+          ? 'bar'
+          : ('pie' as keyof ChartTypeRegistry),
         data: {
           labels: userLabels,
           datasets: [
@@ -62,9 +70,9 @@ this.dataService.optionSubject.subscribe(res=>{
               borderWidth: 2, // Grosor del borde
               borderRadius: 10,
               hoverBorderColor: '#6742F5', // Cambia el color del borde al pasar el cursor
-              hoverBorderWidth: 2
-            }
-          ]
+              hoverBorderWidth: 2,
+            },
+          ],
         },
         options: {
           scales: {
@@ -72,14 +80,14 @@ this.dataService.optionSubject.subscribe(res=>{
               beginAtZero: true,
               title: {
                 display: true,
-                text: `Total ${this.label}`
-              }
+                text: `Total ${this.label}`,
+              },
             },
             x: {
               grid: {
-                display: true // Desactiva las líneas de la cuadrícula horizontal
-              }
-            }
+                display: true, // Desactiva las líneas de la cuadrícula horizontal
+              },
+            },
           },
           plugins: {
             legend: {
@@ -90,16 +98,12 @@ this.dataService.optionSubject.subscribe(res=>{
                 font: {
                   size: 16, // Cambia el tamaño de la fuente de la leyenda
                   weight: 'bold', // Cambia el peso de la fuente de la leyenda
-          
-                }
-              }
-            }
-          }
-        }
+                },
+              },
+            },
+          },
+        },
       });
-      
-    
-
     } else {
       console.error('No se pudo obtener el contexto del lienzo.');
     }
