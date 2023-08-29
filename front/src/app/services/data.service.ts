fb712bdf000ede13c54ae7b47bfa41cbbafe93b2
Emiliano <TE.EA2356@lanvera.org>
2023-08-28 19:05:42 -0600

    fixes

--- front/src/app/services/data.service.ts
+++ front/src/app/services/data.service.ts
@@ -6,25 +6,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-baseUrl = 'http://localhost:6001'
-path =''
- optionSubject = new BehaviorSubject<string>('/users/top_followers/');
+  baseUrl = 'http://localhost:6001'
+  path =''
+  optionSubject = new BehaviorSubject<string>('/users/top_followers/');
   constructor(private http: HttpClient ) { }
 
   getDummy(path: string): Observable<any>{
    return this.http.get(this.baseUrl+path)
-
-
-
-    // return[
-    //   { year: 2010, count: 10 },
-    //   { year: 2011, count: 20 },
-    //   { year: 2012, count: 15 },
-    //   { year: 2013, count: 25 },
-    //   { year: 2014, count: 22 },
-    //   { year: 2015, count: 30 },
-    //   { year: 2016, count: 28 },
-    // ];
   }
 
   getData(){
